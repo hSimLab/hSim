e92643661a74bab9c4187c8eccce5cf939aaa9ca
Andrey Derzhavin <derzhavin.andrej@yandex.ru>
2025-02-17 14:48:08 +0300

    Add brackets

--- src/elf_loader.cc
+++ src/elf_loader.cc
@@ -7,8 +7,9 @@ namespace hsim {
 void ElfLoader::load(Memory &memory) {
     ELFIO::elfio reader{};
 
-    if (!reader.load(m_path))
+    if (!reader.load(m_path)) {
         throw std::invalid_argument("Bad ELF path: " + m_path.string());
+    }
 
     if (reader.get_encoding() != ELFIO::ELFDATA2LSB) {
         throw std::runtime_error("Wrong ELF encoding");
